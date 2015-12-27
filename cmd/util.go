ad2a92bd94434458de682aef8660d2a3169526cb
Darron Froese <darron@froese.org>
2015-12-27 13:41:01 -0600

    Move AutoEnable() to util.go.

--- cmd/util.go
+++ cmd/util.go
@@ -37,3 +37,12 @@ func GetHostname() string {
 	hostname, _ := os.Hostname()
 	return hostname
 }
+
+// AutoEnable helps to automatically enable flags based on cues from the environment.
+func AutoEnable() {
+	// Check for dd-agent configuration file.
+	if _, err := os.Stat("/etc/dd-agent/datadog.conf"); err == nil {
+		DogStatsd = true
+		Log("Auto-enabling Dogstatsd.", "info")
+	}
+}
